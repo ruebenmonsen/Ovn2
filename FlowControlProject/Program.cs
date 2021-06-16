cdc198ce3e882594c1b11ec12a88abe0d6ca94db
Rueben Pakola Monsen <ruebenmonsen@gmail.com>
2021-06-16 22:54:37 +0200

    Added person or pension check method

--- FlowControlProject/Program.cs
+++ FlowControlProject/Program.cs
@@ -11,12 +11,14 @@ namespace FlowControlProject
             do
             {
                 // Main menu with options
+                Console.WriteLine("-------------------------------------------------------------");
                 Console.WriteLine("Du har kommit till huvudmenyn.");
                 Console.WriteLine("Här kan du skriva in siffror för att testa olika funktioner.");
                 Console.WriteLine("1: Ungdom eller pensionär.");
                 Console.WriteLine("2: Upprepa tio gånger.");
                 Console.WriteLine("3: Det tredje ordet.");
                 Console.WriteLine("0: Avsluta");
+                Console.WriteLine("-------------------------------------------------------------");
 
                 // User input
                 int choice = int.Parse(Console.ReadLine()); // Exception handling/validation here
@@ -27,10 +29,31 @@ namespace FlowControlProject
                     case 0:
                         runProgram = false;
                         break;
+                    case 1:
+                        CheckIfPersonOrPension();
+                        break;
                     default:
                         break;
                 }
             } while (runProgram);
+        }
+
+        private static void CheckIfPersonOrPension()
+        {
+            Console.WriteLine("Ange en ålder.");
+            int age = int.Parse(Console.ReadLine());
+            if(age < 20)
+            {
+                Console.WriteLine("Ungdomspris: 80 kr");
+            }
+            else if(age > 65)
+            {
+                Console.WriteLine("Pensionärspris: 90 kr");
+            }
+            else
+            {
+                Console.WriteLine("Standardpris: 120 kr");
+            }
         }
     }
 }
