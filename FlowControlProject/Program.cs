d9f9341721997e71a70039ec028041e6cfc4bfda
Rueben Pakola Monsen <ruebenmonsen@gmail.com>
2021-06-16 22:16:49 +0200

    Add project files.

---
+++ FlowControlProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FlowControlProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runProgram = true;
+            do
+            {
+                Console.WriteLine("Du har kommit till huvudmenyn.");
+                Console.WriteLine("Här kommer du skriva in siffror för att testa olika funktioner.");
+                var choice = Console.ReadLine(); // Exception handling här
+                switch (choice)
+                {
+                    case 0:
+                        runProgram = false;
+                        break;
+                    default:
+                        break;
+                }
+            } while (runProgram);
+        }
+    }
+}
