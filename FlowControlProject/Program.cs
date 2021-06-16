e532d490e833162e246c23bf38ba01b44e1a913d
Rueben Pakola Monsen <ruebenmonsen@gmail.com>
2021-06-16 22:28:55 +0200

    Added: Main menu. Switch expression shell. Bool condition+while endless iteration. All menu items. Documentation comments.

--- FlowControlProject/Program.cs
+++ FlowControlProject/Program.cs
@@ -6,12 +6,22 @@ namespace FlowControlProject
     {
         static void Main(string[] args)
         {
+            // Run program or exit bool
             bool runProgram = true;
             do
             {
+                // Main menu with options
                 Console.WriteLine("Du har kommit till huvudmenyn.");
-                Console.WriteLine("Här kommer du skriva in siffror för att testa olika funktioner.");
-                var choice = Console.ReadLine(); // Exception handling här
+                Console.WriteLine("Här kan du skriva in siffror för att testa olika funktioner.");
+                Console.WriteLine("1: Ungdom eller pensionär.");
+                Console.WriteLine("2: Upprepa tio gånger.");
+                Console.WriteLine("3: Det tredje ordet.");
+                Console.WriteLine("0: Avsluta");
+
+                // User input
+                int choice = int.Parse(Console.ReadLine()); // Exception handling/validation here
+
+                // Menu switch-logic
                 switch (choice)
                 {
                     case 0:
